0850393e0521bf35e01e2bff5ec1d2ba359b8b28
Lisa <wamaithak05@gmail.com>
2024-03-16 16:25:53 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const writeCards= ["Guadalupe", "Ollie", "Aki"];
+
+function writecards(cards) {
+    for(let i=0; i < cards.length; i+++) {
+        console.log('Thank You, ${cards[i]}, for the wonderful surprise gift');
+        debugger;
+    }
+
+    return cards;
+}
+
+writecards(cards);
+
+function countdown() {
+    let countdown = 0;
+    while (countdown <10) {
+        console.log(countdown++)
+    }
+}
